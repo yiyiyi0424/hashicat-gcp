cd9101b259f6a574effb96c517895b1e74fd6733
Liang Ma <yiyi0424@gmail.com>
2023-05-11 21:47:45 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,16 @@
-subnets = [
+module "network" {
+  source  = "app.terraform.io/test_lm/network/google"
+  version = "3.4.0"
+
+  # insert required variables here
+  network_name = "liangma-network"
+  project_id = "var.project"
+  subnets = [
   {
     subnet_name   = "liangma-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
 ]
+~         
+}
