cc3af0fe618aebb9b255546646fee868cca42727
Liang Ma <yiyi0424@gmail.com>
2023-05-11 21:50:48 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -3,11 +3,11 @@ module "network" {
   version = "3.4.0"
 
   # insert required variables here
-  network_name = "liangma-network"
+  network_name = "gaurav-network"
   project_id = "var.project"
   subnets = [
   {
-    subnet_name   = "liangma-subnet"
+    subnet_name   = "gaurav-subnet"
     subnet_ip     = "10.100.10.0/24"
     subnet_region = var.region
   }
